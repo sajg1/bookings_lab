1aecc9e9990dd320cb93428e856cbe0ae6e3f141
Steve Gossip <stephengossip@gmail.com>
2019-07-03 13:24:49 +0100

    App fleshed out

--- client/bookings/src/App.vue
+++ client/bookings/src/App.vue
@@ -1,9 +1,18 @@
 <template lang="html">
-
+  <booking-form />
+  <bookings-grid />
 </template>
 
 <script>
+import BookingForm from '@/components/BookingForm';
+import BookingsGrid from '@/components/BookingsGrid';
+
 export default {
+  name: 'app',
+  components: {
+    'booking-form': BookingForm,
+    'bookings-grid': BookingsGrid
+  }
 }
 </script>
 
